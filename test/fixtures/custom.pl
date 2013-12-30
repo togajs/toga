50d802142e51c4288c9bbe9d99da1ea0bb8848cc
Shannon Moeller <me@shannonmoeller.com>
2013-12-30 18:06:35 -0500

    Added missing test fixture files.

---
+++ test/fixtures/custom.pl
@@ -0,0 +1,35 @@
+use strict;
+use warnings;
+
+print "hello world";
+
+=pod
+
+# Title
+
+Long description that spans multiple
+lines and even has other markdown
+type things.
+
+Like more paragraphs.
+
+* Like
+* Lists
+
+    my $code = "samples";
+
+@arg {Type} name Description.
+@arg {Type} name Description that is really long
+  and wraps to other lines.
+@arg {Type} name Description that is really long
+  and wraps to other lines.
+
+  And has line breaks, etc.
+
+@example
+
+    my $foo = "bar";
+
+@tag
+
+=cut
